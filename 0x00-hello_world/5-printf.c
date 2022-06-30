3b67cf305c4428721d900b638cf69ac88d5deb95
KalkM <106752167+KalkM@users.noreply.github.com>
2022-06-30 14:38:10 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - print a string
+ *Return: puts string
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
