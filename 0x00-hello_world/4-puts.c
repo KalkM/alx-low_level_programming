bbd8d0679c8ffe537b9d1c1f0870882caacb0c31
KalkM <106752167+KalkM@users.noreply.github.com>
2022-06-30 14:34:44 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
-/**
- *
- *  main - print a string
- *
- *  Return: puts string
- */
-
 #include <stdio.h>
-
+/**
+ *main - print a string
+ *Return: puts string
+ */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-
-}  
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
