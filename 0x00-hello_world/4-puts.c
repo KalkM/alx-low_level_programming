3171353b24d82c6643bf495c37e8d5652b1d6f72
KalkM <yidreslekali@gmail.com>
2022-06-30 03:44:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
+/**
+ *
+ *  main - print a string
+ *
+ *  Return: puts string
+ */
+
 #include <stdio.h>
 
 int main(void)
-{
 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 
