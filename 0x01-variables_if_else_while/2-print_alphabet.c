8e57e39587a3de03925d06951166156a5848b96d
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-01 11:56:35 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* prints out the alphabet
+* returns 0
+*/
+int main(void)
+{
+  char ch = 'a';
+	int n;
+  n=0;
+  while (n!=26)
+    {
+        putchar (ch);
+        n++;
+        ch++;
+    if (i == 26)
+        putchar ('\n');
+    }
+return (0);
+}
