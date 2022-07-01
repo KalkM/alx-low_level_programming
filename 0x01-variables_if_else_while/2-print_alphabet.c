c75298d4cccf05da59230f30af10bbe1f37f9b90
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-01 12:10:52 +0300

    prints alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,16 @@
 */
 int main(void)
 {
-  char ch = 'a';
-	int n;
-  n=0;
-  while (n!=26)
-    {
-        putchar (ch);
-        n++;
-        ch++;
-    if (n == 26)
-        putchar ('\n');
-    }
+char ch = 'a';
+int n;
+n=0;
+while (n!=26)
+{
+putchar (ch);
+n++;
+ch++;
+if (n == 26)
+putchar ('\n');
+}
 return (0);
 }
