0c2fee394bdc76c6d2d7c4392397e2b5488e4813
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-01 12:27:03 +0300

    print the alphabet in lowercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase except q and e
+ *Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+int n;
+n = 0;
+while (n != 26 || n != 4 || n != 16)
+{
+putchar (ch);
+n++;
+ch++;
+if (n == 26)
+putchar ('\n');
+}
+return (0);
+}
