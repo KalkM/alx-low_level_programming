db869e521b120cf03bbd81213217997e180bbbd9
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-05 12:56:20 +0300

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *jack_bauer - Do something
+ *Return: Return something
+ */
+void jack_bauer(void)
+{
+int i, j, k, x;
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (x = 0; x < 10; x++)
+{
+if (i == 2 && j == 4 && k == 0 && x == 0)
+{
+return;
+}
+else
+{
+_putchar((char)(i + 48));
+_putchar((char)(j + 48));
+_putchar(58);
+_putchar((char)(k + 48));
+_putchar((char)(x + 48));
+_putchar('\n');
+}
+}
+}
+}
+}
+}
