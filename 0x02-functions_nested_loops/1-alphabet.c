1a492ea693cb31eef1b78e36178d6bd90cee0f30
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-05 08:09:12 +0300

    prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - Do something
+ *Return: Return something
+ */
+void print_alphabet(void)
+{
+char alp = 'a';
+while (alp <= 'z')
+{
+_putchar(alp);
+alp++;
+}
+_putchar('\n');
+}
