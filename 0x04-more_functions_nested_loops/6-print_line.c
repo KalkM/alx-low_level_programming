32be5fe1668375739453538f2d846e50863a333a
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-07 15:39:03 +0300

    prints lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  *print_line- prints _ n times
- @n: number of _
+ *@n: number of _
  * Return: something
  */
 void print_line(int n)
 {
-int i, n;
+int i;
 if (n >= 0)
 putchar('\n');
 for (i = 0; i < n; i++)
