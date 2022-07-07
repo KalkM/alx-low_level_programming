6d06316a0b54496b0570eff86b7e746becd9c87c
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-07 15:53:55 +0300

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *print_line- prints _ n times
- *@n: number of _
- * Return: something
- */
+*print_line- prints _ n times
+*@n: number of _
+* Return: something
+*/
 void print_line(int n)
 {
 int i;
