2de6abdea6858d3024bbf4b5a954a9a5f7866053
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-11 11:27:35 +0300

    counts the length of the string

---
+++ 0x05-pointers_arrays_strings/-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strlen - counts the length of the string
+ *@s: accepts string
+ *Return: the count
+ */
+int _strlen(char *s)
+{
+int i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
