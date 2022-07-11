ee178b53a2568280881e74c05bb70d0f0480a433
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-11 14:17:22 +0300

    prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@
  */
 void puts2(char *str)
 {
-int i, j;
-i = 0;
-while (str[i] != '\0')
-i++;
-for (j = 0; j < i; j++)
-_putchar(str[j]);
+int i = 0;
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
 _putchar('\n');
 }
