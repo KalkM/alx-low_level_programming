281f4725f8b71139aa7dbfc88e4c7922d27f68db
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-11 11:23:52 +0300

    swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+/**
+ * swap_int- swaps two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+
+{
+
+int c;
+c = *a;
+*a = *b;
+*b = c;
+
+}
