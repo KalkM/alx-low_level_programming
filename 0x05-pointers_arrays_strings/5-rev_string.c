ab0361726a04edc1eef53c437eae38ac27ac0438
KalkM <106752167+KalkM@users.noreply.github.com>
2022-07-11 13:46:21 +0300

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,17 @@
  */
 void rev_string(char *s)
 {
-int i, j, x, y;
+int i, j, k;
 char c[1024];
-y = 0;
 i = 0;
-for (i = 0; s[i] != '\0'; i++)
+while (s[i] != '\0')
+i++;
+j = i - 1;
+for (k = 0; k < i; k++)
 {
+c[k] = s[j];
+j--;
 }
-i--;
-for (j = i; j > -1; j--)
-{
-c[j] = s[j];
-y++;
-}
-for (x = 0; x <= i; x++)
-{
-s[x] = c[x];
-}
+
+ c[i] = '\0';
 }
