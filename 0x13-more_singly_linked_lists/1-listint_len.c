10f201fb8ea4d8e9c508bcdc1809e7f1a02faad2
KalkM <106752167+KalkM@users.noreply.github.com>
2022-08-15 20:48:22 +0300

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- * in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
- * Return: The number of elements in the listint_t list.
+ * listint_len - returns the lenth of a list;
+ * @h: pointer to the head of a list.
+ * Return: Length (INT) of a list.
  */
- 
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+  size_t n_nodes = 0;
 
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
+  if (!h)
+    return (0);
 
-	return (nodes);
+  while (h)
+  {
+    n_nodes++;
+    h = h->next;
+  }
+  return (n_nodes);
 }
