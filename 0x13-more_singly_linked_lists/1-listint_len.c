e5bc8b511d9580e92dd054fd046c3d256cb4658f
KalkM <106752167+KalkM@users.noreply.github.com>
2022-08-16 05:13:55 +0300

    Length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the lenth of a list;
- * @h: pointer to the head of a list.
- * Return: Length (INT) of a list.
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ * Return: the number of elements
  */
+
 size_t listint_len(const listint_t *h)
 {
-  size_t n_nodes = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-  if (!h)
-    return (0);
-
-  while (h)
-  {
-    n_nodes++;
-    h = h->next;
-  }
-  return (n_nodes);
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
